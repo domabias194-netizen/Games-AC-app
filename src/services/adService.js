401fb96fb161897b5c4c31000e4b986e6eff1a3f
domabias194-netizen <domabias194@gmail.com>
2026-02-21 15:18:14 +0100

    Create Google AdMob integration service

---
+++ src/services/adService.js
@@ -0,0 +1,25 @@
+// adService.js
+
+class AdService {
+    constructor() {
+        // Initialize AdMob here
+    }
+
+    loadInterstitialAd(adUnitId) {
+        // Load an interstitial ad
+    }
+
+    showInterstitialAd() {
+        // Show the loaded interstitial ad
+    }
+
+    loadRewardedAd(adUnitId) {
+        // Load a rewarded ad
+    }
+
+    showRewardedAd() {
+        // Show the loaded rewarded ad
+    }
+}
+
+export default new AdService();

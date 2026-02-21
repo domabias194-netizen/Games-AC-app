2700e6985858b29ab6e1d0d18a39b6041fdbcc40
domabias194-netizen <domabias194@gmail.com>
2026-02-21 15:18:58 +0100

    Create GameBoard display component

---
+++ src/components/GameBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameBoard = () => {
+    return (
+        <div className="game-board">
+            {/* Game board layout here */}
+        </div>
+    );
+};
+
+export default GameBoard;

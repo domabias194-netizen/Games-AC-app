91ff72d0844abcc566b68fd3d257a959a21a1315
domabias194-netizen <domabias194@gmail.com>
2026-02-21 15:18:44 +0100

    Create AdBanner component

---
+++ src/components/AdBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdBanner = () => {
+    return (
+        <div>
+            <img src="/path-to-your-advertisement-image.jpg" alt="Advertisement" />
+        </div>
+    );
+};
+
+export default AdBanner;
